91ac35ba44638d5d1c63f991dbbdec205594208d
mogitfamcap <mogitfamcap@gmail.com>
2015-08-28 09:02:27 -0700

    Updated table layout

--- hvcm.js
+++ hvcm.js
@@ -10,8 +10,16 @@ function drawVideos() {
     videosDb.getAll(function(videos) {
         videos.forEach(function(v) {
             var name = video.name(v.path);
-            var nameHref = '<a href="#" onclick="onVideoClicked(' + v.id + ');">' + name + '</a>';
-            result += "<tr><td>" + nameHref + "</td><td>" + v.path + "</td></tr>";
+            var play = '<a href="#" onclick="onVideoClicked(' + v.id + ');">' + 'Play' + '</a>';
+            result += "<tr>"
+            result += "<td>" + play + "</td>";
+            result += "<td>" + name + "</td>";
+            result += "<td>" + '' + "</td>"; // Tags
+            result += "<td>" + '' + "</td>"; // Cast
+            result += "<td>" + formatTimestamp(v.last_opened_at) + "</td>";
+            result += "<td>" + formatTimestamp(v.created_at) + "</td>";
+            result += "<td>" + formatTimestamp(v.added_at) + "</td>";
+            result += "</tr>"
         });
         $('#videos-table-body').html(result);
     });
@@ -26,4 +34,27 @@ function onVideoClicked(id) {
         exec(cmd, function(error, stdout, stderr) {
         });
     });
+}
+
+function formatTimestamp(timestamp) {
+    if (!timestamp) {
+        return "";
+    }
+    var date = new Date(timestamp * 1000);
+
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+
+    month = (month < 10 ? "0" : "") + month;
+    day = (day < 10 ? "0" : "") + day;
+    hour = (hour < 10 ? "0" : "") + hour;
+    min = (min < 10 ? "0" : "") + min;
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var str = date.getFullYear() + "-" + month + "-" + day + "_" +  hour + ":" + min + ":" + sec;
+
+    return str;
 }
