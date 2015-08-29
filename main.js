2abd96797324c4ace8af3dd9b1339b52f0b7d9a7
mogitfamcap <mogitfamcap@gmail.com>
2015-08-29 12:53:27 -0700

    Full size window

--- main.js
+++ main.js
@@ -21,7 +21,9 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  var atomScreen = require('screen');
+  var size = atomScreen.getPrimaryDisplay().workAreaSize;
+  mainWindow = new BrowserWindow({ width: size.width, height: size.height });
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
